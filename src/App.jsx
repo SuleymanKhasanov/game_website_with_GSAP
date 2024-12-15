c2873769ad3b6f4330850ea95287d8c628defd66
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-12-15 18:28:54 +0500

    Added peculiarities section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Aboute from './components/Aboute';
 import Hero from './components/Hero';
 import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
         <Navbar/>
-      <Hero />
-      <Aboute />
+        <Hero />
+        <Aboute />
+        <Features />
     </main>
   );
 }
