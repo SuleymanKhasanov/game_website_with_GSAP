46d104117924b70a1fcd4358583ec5d54a5cfbff
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-12-16 13:33:13 +0500

    Added contacts section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar.jsx';
 import Features from './components/Features.jsx';
 import Story from './components/Story.jsx';
+import Contacts from "./components/Contacts.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Aboute />
       <Features />
       <Story />
+        <Contacts/>
     </main>
   );
 }
